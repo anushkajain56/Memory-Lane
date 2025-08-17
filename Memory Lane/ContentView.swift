0ed2dd717fae2569462c5967ce8699ffcb0866fc
Anushka <anushka.jain2006@outlook.com>
2025-08-16 21:22:03 -0400

    Initial Commit

---
+++ Memory Lane/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memory Lane
+//
+//  Created by Anushka Jain on 8/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memory Lane/Memory_LaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_LaneApp.swift
+//  Memory Lane
+//
+//  Created by Anushka Jain on 8/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Memory_LaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
